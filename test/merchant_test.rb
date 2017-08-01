8998a104fe54e0c471127ddbe901c46491c8b660
Boots2291 <buteramv@gmail.com>
2017-08-01 14:51:01 -0600

    Fix time parsing issue on tests

--- test/merchant_test.rb
+++ test/merchant_test.rb
@@ -4,33 +4,33 @@ require './lib/merchant'
 class MerchantTest < Minitest::Test
 
   def test_it_exists
-    merchant = Merchant.new(1, "Bill", "Yesterday", "Today", self)
+    merchant = Merchant.new(1, "Bill", "2010-12-10", "2011-12-04", self)
 
     assert_instance_of Merchant, merchant
   end
 
   def test_it_has_an_id
-    merchant = Merchant.new(1, "Bill", "Yesterday", "Today", self)
+    merchant = Merchant.new(1, "Bill", "2010-12-10", "2011-12-04", self)
 
     assert_equal 1, merchant.id
   end
 
   def test_it_has_a_name
-    merchant = Merchant.new(1, "Bill", "Yesterday", "Today", self)
+    merchant = Merchant.new(1, "Bill", "2010-12-10", "2011-12-04", self)
 
     assert_equal "Bill", merchant.name
   end
 
   def test_it_knows_when_created
-    merchant = Merchant.new(1, "Bill", "Yesterday", "Today", self)
+    merchant = Merchant.new(1, "Bill", "2010-12-10", "2011-12-04", self)
 
-    assert_equal "Yesterday", merchant.created_at
+    assert_equal Time.parse("2010-12-10"), merchant.created_at
   end
 
   def test_it_knows_when_updated
-    merchant = Merchant.new(1, "Bill", "Yesterday", "Today", self)
+    merchant = Merchant.new(1, "Bill", "2010-12-10", "2011-12-04", self)
 
-    assert_equal "Today", merchant.updated_at
+    assert_equal Time.parse("2011-12-04"), merchant.updated_at
   end
 
 end
